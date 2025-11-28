ab550af604f8f6b47391db4184065cebbf5cc127
kaien <kaienkira@gmail.com>
2025-11-28 22:04:20 +0800

    work sync

--- compiler/src/main.go
+++ compiler/src/main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, ""+
+		"brickred exchange compiler\n"+
+		"usage: %s "+
+		"-f <protocol_file> "+
+		"-l <language>"+
+		"\n"+
+		"    [-o <output_dir>]\n"+
+		"    [-I <search_path>]\n"+
+		"    [-n <new_line_type>] (unix|dos) default is unix\n"+
+		"language supported: cpp php csharp\n",
+		filepath.Base(os.Args[0]))
+}
+
 func main() {
+	optProtoFilePath := flag.String("f", "", "")
+	optLanguage := flag.String("l", "", "")
+	flag.Parse()
+
+	if *optProtoFilePath == "" || *optLanguage == "" {
+		printUsage()
+		os.Exit(1)
+	}
 }
