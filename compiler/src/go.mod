3e1d8dd360ab41129cda89ac955f270ff3ed096a
kaien <kaienkira@gmail.com>
2025-11-29 21:53:44 +0800

    work sync

--- compiler/src/go.mod
+++ compiler/src/go.mod
@@ -2,4 +2,11 @@ module github.com/kaienkira/brickred-exchange-v3/compiler
 
 go 1.25
 
-require github.com/spf13/pflag v1.0.10 // indirect
+require (
+	github.com/antchfx/xmlquery v1.5.0 // indirect
+	github.com/antchfx/xpath v1.3.5 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
---
+++ compiler/src/protocol_descriptor.go
@@ -0,0 +1,214 @@
+package main
+
+type ProtocolDescriptor struct {
+	ProtoDef *ProtocolDef
+
+	// ProtocolDef.Name -> ProtocolDef
+	ImportedProtos map[string]*ProtocolDef
+}
+
+// ----------------------------------------------------------------------------
+type ProtocolDef struct {
+	Name     string
+	FilePath string
+
+	// import define
+	// in file define order
+	Imports []*ImportDef
+	// ImportDef.Name -> ImportDef
+	ImportNameIndex map[string]*ImportDef
+
+	// namespace define
+	// language -> NamespaceDef
+	Namespaces map[string]*NamespaceDef
+
+	// enum define
+	// in file define order
+	Enums []*EnumDef
+	// EnumDef.Name -> EnumDef
+	EnumNameIndex map[string]*EnumDef
+
+	// struct define
+	// in file define order
+	Structs []*StructDef
+	// StructDef.Name -> StructDef
+	StructNameIndex map[string]*StructDef
+
+	// enum map define
+	// in file define order
+	EnumMaps []*EnumMapDef
+	// EnumMapDef.Name -> EnumMapDef
+	EnumMapNameIndex map[string]*EnumMapDef
+}
+
+// ----------------------------------------------------------------------------
+type ImportDef struct {
+	// link to parent define
+	ParentRef *ProtocolDef
+	// import name
+	Name string
+	// define in line number
+	LineNumber int
+
+	ProtoDef *ProtocolDef
+
+	// import is referenced by enum
+	IsRefByEnum bool
+	// import is referenced by struct
+	IsRefByStruct bool
+	// import is referenced by enum map
+	IsRefByEnumMap bool
+}
+
+// ----------------------------------------------------------------------------
+type NamespaceDef struct {
+	// link to parent define
+	ParentRef *ProtocolDef
+	// namespace for language
+	Language string
+	// define in line number
+	LineNumber int
+
+	Namespace      string
+	NamespaceParts []string
+}
+
+// ----------------------------------------------------------------------------
+type EnumDef struct {
+	// link to parent define
+	ParentRef *ProtocolDef
+	// enum name
+	Name string
+	// define in line number
+	LineNumber int
+
+	// in file define order
+	Items []*EnumItemDef
+	// EnumItemDef.Name -> EnumItemDef
+	ItemNameIndex map[string]*EnumItemDef
+}
+
+type EnumDefItemType int
+
+const (
+	EnumDefItemType_None EnumDefItemType = iota
+	EnumDefItemType_Default
+	EnumDefItemType_Int
+	EnumDefItemType_CurrentEnumRef
+	EnumDefItemType_OtherEnumRef
+)
+
+type EnumItemDef struct {
+	// link to parent define
+	ParentRef *EnumDef
+	// enum item name
+	Name string
+	// define in line number
+	LineNumber int
+
+	Type           EnumDefItemType
+	IntValue       int
+	RefEnumItemDef *EnumItemDef
+}
+
+// ----------------------------------------------------------------------------
+type StructDef struct {
+	// link to parent define
+	ParentRef *ProtocolDef
+	// struct name
+	Name string
+	// define in line number
+	LineNumber int
+
+	// in file define order
+	Fields []*StructFieldDef
+	// StructFieldDef.Name -> StructFieldDef
+	FieldNameIndex map[string]*StructFieldDef
+
+	OptionalFieldCount int
+	OptionalByteCount  int
+}
+
+type StructFieldType int
+
+const (
+	StructFieldType_None StructFieldType = iota
+	StructFieldType_I8
+	StructFieldType_U8
+	StructFieldType_I16
+	StructFieldType_U16
+	StructFieldType_I32
+	StructFieldType_U32
+	StructFieldType_I64
+	StructFieldType_U64
+	StructFieldType_I16V
+	StructFieldType_U16V
+	StructFieldType_I32V
+	StructFieldType_U32V
+	StructFieldType_I64V
+	StructFieldType_U64V
+	StructFieldType_String
+	StructFieldType_Bytes
+	StructFieldType_Bool
+	StructFieldType_Enum
+	StructFieldType_Struct
+	StructFieldType_List
+)
+
+type StructFieldDef struct {
+	// link to parent define
+	ParentRef *StructDef
+	// field name
+	Name string
+	// define in line number
+	LineNumber int
+
+	Type               StructFieldType
+	ListType           StructFieldType
+	RefEnumDef         *EnumDef
+	RefStructDef       *StructDef
+	IsOptional         bool
+	OptionalFieldIndex int
+}
+
+// ----------------------------------------------------------------------------
+type EnumMapDef struct {
+	// link to parent define
+	ParentRef *ProtocolDef
+	// enum map name
+	Name string
+	// define in line number
+	LineNumber int
+
+	// in file define order
+	Items []*EnumMapItemDef
+	// EnumMapItemDef.Name -> EnumMapItemDef
+	ItemNameIndex map[string]*EnumMapItemDef
+	// StructId -> StructDef
+	IdToStructIndex map[int]*StructDef
+	// StructDef -> StructId
+	StructToIdIndex map[*StructDef]int
+}
+
+type EnumMapItemType int
+
+const (
+	EnumMapItemType_None EnumMapItemType = iota
+	EnumMapItemType_Default
+	EnumMapItemType_Int
+	EnumMapItemType_CurrentEnumRef
+)
+
+type EnumMapItemDef struct {
+	// link to parent define
+	ParentRef *EnumMapDef
+	// enum map item name
+	Name string
+	// define in line number
+	LineNumber int
+
+	Type           EnumMapItemType
+	IntValue       int
+	RefEnumItemDef *EnumMapItemDef
+	RefStructDef   StructDef
+}
